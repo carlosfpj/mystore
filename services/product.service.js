46f4d6cc8f061aa204230655b4dfe9715e36a93b
carlosfpj <cafer89@gmail.com>
2023-08-20 18:51:17 -0500

    environment variables

--- services/product.service.js
+++ services/product.service.js
@@ -1,10 +1,15 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
+
+const pool = require('../libs/postgres.pool');
+
 class ProductsService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.log(err))
   }
 
   generate() {
@@ -29,12 +34,10 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-        res(this.products)
-      }, 4000);
-    })
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id) {
