7544fa9d23f686e468a6dde76014b30b6d80fa63
carlosfpj <cafer89@gmail.com>
2023-08-26 17:48:33 -0500

    sequelize

--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
-
-const pool = require('../libs/postgres.pool');
-
+// const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 class ProductsService {
 
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err))
+    // this.pool = pool;
+    // this.pool.on('error', (err) => console.log(err))
   }
 
   generate() {
@@ -36,8 +35,11 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    // const rta = await this.pool.query(query);
+    const [data] = await sequelize.query(query);
+    return {
+      data
+    };
   }
 
   async findOne(id) {
