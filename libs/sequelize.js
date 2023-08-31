4e86f66c7b1c0349989c847566d60a968ed629f2
carlosfpj <cafer89@gmail.com>
2023-08-30 20:09:02 -0500

    sequelize v2

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,14 +1,18 @@
 const {Sequelize} = require('sequelize');
 
 const {config} = require('./../config/config');
+const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequielize = new Sequelize(URI, {
+const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: true,
 });
 
-module.exports = sequielize;
+setupModels(sequelize);
+sequelize.sync();
+
+module.exports = sequelize;
