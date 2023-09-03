4a8ad58f67ff7ac0b0737833b2d133c940924c6d
carlosfpj <cafer89@gmail.com>
2023-09-03 11:41:44 -0500

    switch to mysql database orm

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routerApi = require('./routes');
 const cors = require('cors');
 
-const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
+const {logErrors, errorHandler, boomErrorHandler, ormErrorHandler} = require('./middlewares/error.handler')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -31,6 +31,7 @@ app.get('/api/nueva-ruta', (req, res) => {
 
 routerApi(app);
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
