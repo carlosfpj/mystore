40e5e438f7b14920f853f4bfaa955208f9368f56
carlosfpj <cafer89@gmail.com>
2023-09-11 20:07:35 -0500

    1-on-1 relations

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,11 @@
 const {User, userSchema} = require('./user.model');
+const {Customer, CustomerSchema} = require('./customer.model');
 
 function setupModels(sequelize) {
   User.init(userSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
+const customersRouter = require('./customers.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/customers', customersRouter);
   router.use('/users', usersRouter)
 }
 
