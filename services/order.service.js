64d97cdc850af6d0ccd46492c4c2a65631343770
carlosfpj <cafer89@gmail.com>
2023-10-10 21:28:11 -0500

    many to many db relationship

---
+++ services/order.service.js
@@ -0,0 +1,16 @@
+const boom = require('@hapi/boom');
+
+const {models} = require('../libs/sequelize');
+
+
+class OrderService {
+
+  constructor() {}
+
+  async create(data) {
+    const newOrder = await models.Order.create(data);
+    return newOrder;
+  }
+}
+
+module.exports = {OrderService}
